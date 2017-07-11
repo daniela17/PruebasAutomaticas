84d965d6226559c01c389153a3ee19ae5b8912bd
Daniela Sofron <daniela.sofron.contractor@bbva.com>
2017-07-11 12:49:30 +0200

    Initial commit

---
+++ PruebasAutomaticas/PruebasAutomaticas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebasAutomaticas
+//
+//  Created by Daniela Sofron on 7/11/17.
+//  Copyright © 2017 Daniela Sofron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebasAutomaticas/PruebasAutomaticas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PruebasAutomaticas
+//
+//  Created by Daniela Sofron on 7/11/17.
+//  Copyright © 2017 Daniela Sofron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
